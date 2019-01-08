af1f9d51ad5f6083c8675068a1fe9fddf046de93
unknown <thanhvuong6499@gmail.com>
2019-01-08 23:55:04 +0700

    table view

--- src/Views/Controllers/AddMemberForProjectController.java
+++ src/Views/Controllers/AddMemberForProjectController.java
@@ -5,6 +5,7 @@
  */
 package Views.Controllers;
 
+import Backend.Model.Nguoidung;
 import com.jfoenix.controls.JFXTextField;
 import java.lang.reflect.Array;
 import java.net.URL;
@@ -12,11 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -27,9 +30,9 @@ import javafx.scene.input.MouseEvent;
 public class AddMemberForProjectController implements Initializable {
 
     @FXML
-    private TableView<String> tblAllMember;
+    private TableView<Nguoidung> tblAllMember;
     @FXML
-    private TableColumn<?, ?> colNameMember;
+    private TableColumn<Nguoidung, String> colNameMember;
     @FXML
     private TableView<?> tblProjectView;
     @FXML
@@ -37,19 +40,32 @@ public class AddMemberForProjectController implements Initializable {
     @FXML
     private JFXTextField txtTimKiem;
 
+    private Nguoidung nguoidung;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        List<String>  list = new ArrayList<>();
-        for(int i = 0; i <6; i++ ){
-            String str = String.format("dcm khoi %d",i);
-            list.add(str);
-        }
+     colNameMember.setCellValueFactory(new PropertyValueFactory<>("hoTen"));
+       ObservableList<Nguoidung> list = getUserList();
+      tblAllMember.setItems(list);
+ 
+      tblAllMember.getColumns().add(colNameMember);
     }    
 
+     private ObservableList<Nguoidung> getUserList() {
+ 
+      Nguoidung user1 = new Nguoidung( "1","asdasd", "asdasd@gmail.com", 
+              "", "", true);
+      Nguoidung user2 = new Nguoidung("2", "mcneil", "mcneil@gmail.com", 
+              "", "", true);
+      Nguoidung user3 = new Nguoidung("3", "asdasdas", "asdasdasdasd@gmail.com", 
+              "", "", false);
+ 
+     ObservableList<Nguoidung> list = FXCollections.observableArrayList(user1, user2, user3);
+      return list;
+  }
     @FXML
     private void btnSearch(MouseEvent event) {
     }
