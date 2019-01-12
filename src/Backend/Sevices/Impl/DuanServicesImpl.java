321740ffd4d83e2b3db91632d94e117cc071aa85
unknown <ntquang22298@gmail.com>
2019-01-13 01:24:08 +0700

    service

--- src/Backend/Sevices/Impl/DuanServicesImpl.java
+++ src/Backend/Sevices/Impl/DuanServicesImpl.java
@@ -6,19 +6,76 @@
 package Backend.Sevices.Impl;
 
 import Backend.Controller.DuanJpaController;
+import Backend.Controller.exceptions.IllegalOrphanException;
+import Backend.Controller.exceptions.NonexistentEntityException;
 import Backend.Model.Duan;
 import Backend.Sevices.DuanServices;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Quang
  */
-public class DuanServicesImpl implements DuanServices{
+public class DuanServicesImpl implements DuanServices {
+
     DuanJpaController duAnController = new DuanJpaController();
+
     @Override
     public List<Duan> getAllByStatus(int idNguoiDung, String trangThaiDuAn) {
         return duAnController.getAllByStatus(idNguoiDung, trangThaiDuAn);
     }
-    
+
+    @Override
+    public void create(String id, String name, Date start, String status) {
+
+        try {
+            duAnController.create(new Duan(id, name, start, status));
+        } catch (Exception ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void edit(String id, String name, Date start, String status) {
+        try {
+            Duan da = duAnController.findDuan(id);
+            da.setTenDuAn(name);
+            da.setNgayBatDau(start);
+            da.setTrangThai(status);
+            duAnController.edit(da);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+        try {
+            duAnController.destroy(id);
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void setEndDate(String id,Date end) {
+        Duan da = duAnController.findDuan(id);
+        da.setNgayKetThuc(end);
+        try {
+            duAnController.edit(da);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(DuanServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
--- src/Backend/Sevices/Impl/NoidungServiceImpl.java
+++ src/Backend/Sevices/Impl/NoidungServiceImpl.java
@@ -5,20 +5,70 @@
  */
 package Backend.Sevices.Impl;
 
+import Backend.Controller.DuanJpaController;
 import Backend.Controller.NoidungJpaController;
+import Backend.Controller.exceptions.NonexistentEntityException;
 import Backend.Model.Noidung;
 import Backend.Sevices.NoidungServices;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Quang
  */
-public class NoidungServiceImpl implements NoidungServices{
+public class NoidungServiceImpl implements NoidungServices {
+
     NoidungJpaController noiDungController = new NoidungJpaController();
+    DuanJpaController duAnController = new DuanJpaController();
+
     @Override
-    public List<Noidung> getAllByStatus(String idDuAn,String trangThaiTask) {
+    public List<Noidung> getAllByStatus(String idDuAn, String trangThaiTask) {
         return noiDungController.getAllByStatus(idDuAn, trangThaiTask);
     }
-    
+
+    @Override
+    public void create(String iDDuAn, String iDNoiDung, String tieuDe, String noiDung, String trangThai, Date ngayBatDau, Date ngayKetThuc) {
+        try {
+            Noidung nd = new Noidung();
+            nd.setIDNoiDung(iDNoiDung);
+            nd.setTieuDe(tieuDe);
+            nd.setNoiDung(noiDung);
+            nd.setTrangThai(trangThai);
+            nd.setNgayBatDau(ngayBatDau);
+            nd.setNgayKetThuc(ngayKetThuc);
+            nd.setIDDuAn((duAnController.findDuan(iDDuAn)));
+            noiDungController.create(nd);
+        } catch (Exception ex) {
+            Logger.getLogger(NoidungServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void edit(String iDDuAn,String iDNoiDung, String tieuDe, String noiDung, String trangThai, Date ngayBatDau, Date ngayKetThuc) {
+        Noidung nd = noiDungController.findNoidung(iDNoiDung);
+        nd.setTieuDe(tieuDe);
+        nd.setNoiDung(noiDung);
+        nd.setTrangThai(trangThai);
+        nd.setNgayBatDau(ngayBatDau);
+        nd.setNgayKetThuc(ngayKetThuc);
+        nd.setIDDuAn((duAnController.findDuan(iDDuAn)));
+        try {
+            noiDungController.edit(nd);
+        } catch (Exception ex) {
+            Logger.getLogger(NoidungServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void delete(String iDNoiDung) {
+        try {
+            noiDungController.destroy(iDNoiDung);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(NoidungServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
