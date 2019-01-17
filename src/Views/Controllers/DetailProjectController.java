b252b035f47e7b7912519788167d200064722334
Zaffran <biprono12@gmail.com>
2019-01-17 21:00:56 +0700

    Khoi backend

--- src/Views/Controllers/DetailProjectController.java
+++ src/Views/Controllers/DetailProjectController.java
@@ -5,14 +5,22 @@
  */
 package Views.Controllers;
 
+import Backend.Enum.TRANGTHAITASK;
 import Backend.Model.Duan;
+import Backend.Model.Nguoidung;
+import Backend.Model.Noidung;
 import Backend.Sevices.Impl.NoidungServiceImpl;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -47,11 +55,55 @@ public class DetailProjectController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        NoidungServiceImpl ndS = new NoidungServiceImpl();
+        try {
+            // TODO
+            loadForm();
+        } catch (IOException ex) {
+            Logger.getLogger(DetailProjectController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         
         
     }    
+    public void loadForm() throws IOException{
+        NoidungServiceImpl ndS = new NoidungServiceImpl();
+        List<Noidung> lstToDo = ndS.getAllByStatus(lbID.getText(), TRANGTHAITASK.CANLAM.toString());
+        List<Noidung> lstInProgress = ndS.getAllByStatus(lbID.getText(), TRANGTHAITASK.DANGLAM.toString());
+        List<Noidung> lstSolved = ndS.getAllByStatus(lbID.getText(), TRANGTHAITASK.CHODUYET.toString());
+        List<Noidung> lstDone = ndS.getAllByStatus(lbID.getText(), TRANGTHAITASK.HOANTHANH.toString());
+        
+        for (Noidung noidung : lstToDo) {
+            vbToDo.getChildren().add(gen(noidung.getIDNoiDung(), noidung.getTieuDe(), "lol"));
+            Collection<Nguoidung> nguoidungCollection = noidung.getNguoidungCollection();
+            for (Nguoidung nguoidung : nguoidungCollection) {
+                System.out.println(nguoidung.getEmail());
+            }
+            
+        }
+        for (Noidung noidung : lstInProgress) {
+//            gen(noidung.getIDNoiDung(), noidung.getTieuDe(), "lol");
+        }
+        for (Noidung noidung : lstSolved) {
+//            gen(noidung.getIDNoiDung(), noidung.getTieuDe(), "lol");
+        }
+        for (Noidung noidung : lstDone) {
+//            gen(noidung.getIDNoiDung(), noidung.getTieuDe(), "lol");
+        }
+      
+        
+        
+    }
+    public Node gen(String id, String tittle, String userName) throws IOException{
+         Node node = (Node) FXMLLoader.load(getClass().getResource("/Views/panelMission.fxml"));
+            Label lbID = (Label) node.lookup("#lbIDTask");
+            lbID.setText(id);
+            Label lbTitle = (Label) node.lookup("#lbTitleTask");
+            lbTitle.setText(tittle);
+            Label lbUserName = (Label) node.lookup("#lbUserName");
+            lbUserName.setText(userName);
+            return node;
+    }
+    
     public void initData(Duan DA){
 //        setDa(DA);
     }
