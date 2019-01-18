af4e11a74eaabe9d543e7c8f5d15fd38db332177
unknown <thanhvuong6499@gmail.com>
2019-01-19 01:03:10 +0700

    checkcbobox

--- src/Views/Controllers/AddMissionForProjectController.java
+++ src/Views/Controllers/AddMissionForProjectController.java
@@ -10,10 +10,14 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
+import org.controlsfx.control.CheckComboBox;
 
 /**
  * FXML Controller class
@@ -33,6 +37,8 @@ public class AddMissionForProjectController implements Initializable {
     @FXML
     private DatePicker dtNgayKetThuc;
     @FXML
+    private CheckComboBox<String> ckcbMember;
+    @FXML
     private JFXComboBox<?> cbTrangThaiNV;
     @FXML
     private JFXTextField txtMaNhiemVu;
@@ -43,11 +49,22 @@ public class AddMissionForProjectController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        final ObservableList<String> strings = FXCollections.observableArrayList();
+        for (int i = 0; i <= 10; i++) {
+            strings.add("Item " + i);
+        }
+        ckcbMember.getItems().addAll(strings);
+        ckcbMember.getCheckModel().getCheckedItems().addListener(new ListChangeListener<String>(){
+            @Override
+            public void onChanged(ListChangeListener.Change<? extends String> c) {
+                    System.out.println(ckcbMember.getCheckModel().getCheckedItems());            }
+        
+        });
+    }
 
     @FXML
     private void btnThemNhiemVu(MouseEvent event) {
     }
 
-    
+
 }
