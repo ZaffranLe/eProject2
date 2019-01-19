c8130049d6e1145f3eb03ea01f0a82ede440c107
khoidz1999 <vukhoi20021999@gmail.com>
2019-01-19 13:01:07 +0700

    khoidz

--- src/Views/Controllers/DetailProjectController.java
+++ src/Views/Controllers/DetailProjectController.java
@@ -15,6 +15,7 @@ import Backend.Sevices.Impl.NguoidungServicesImpl;
 import Backend.Sevices.Impl.NoidungServiceImpl;
 import Backend.Sevices.Impl.UserSevicesImpl;
 import Foundation.Transdata;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
@@ -54,27 +55,36 @@ public class DetailProjectController implements Initializable {
     @FXML
     private Label lbNameProject;
     @FXML
+    private Label lbEndDay;
+    @FXML
+    private JFXComboBox<bindDataComboBoxMemBer> cbMember;
+    @FXML
+    private Label lbMember;
+    @FXML
     private JFXTextField txtSearch;
-
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        NguoidungServicesImpl userS = new NguoidungServicesImpl();
+        List<Nguoidung> allByProject = userS.getAllByProject(Transdata.Instance().getProjectID());
+        for (Nguoidung nguoidung : allByProject) {
+            cbMember.getItems().add(new bindDataComboBoxMemBer(nguoidung.getId(), nguoidung.getHoTen()));
+        }
         try {
             loadForm();
         } catch (IOException ex) {
             Logger.getLogger(DetailProjectController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        
-    }    
-    public void loadForm() throws IOException{
+
+    }
+
+    public void loadForm() throws IOException {
         clearVbox();
         NoidungServiceImpl ndS = new NoidungServiceImpl();
-        List<Noidung> lstTask  = ndS.getAllByDuAn(Transdata.Instance().getProjectID());
+        List<Noidung> lstTask = ndS.getAllByDuAn(Transdata.Instance().getProjectID());
         NguoidungServicesImpl userS = new NguoidungServicesImpl();
         for (Noidung noidung : lstTask) {
             if (noidung.getTrangThai().equals(TRANGTHAITASK.CANLAM.toString())) {
@@ -82,7 +92,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbToDo.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -91,7 +101,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbInProgress.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -100,7 +110,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbSolved.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -109,23 +119,25 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbComplete.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
         }
-        
+
     }
-    public void clearVbox(){
+
+    public void clearVbox() {
         vbToDo.getChildren().clear();
         vbInProgress.getChildren().clear();
         vbSolved.getChildren().clear();
         vbComplete.getChildren().clear();
     }
-    public void loadFormByEmail() throws IOException{
+
+    public void loadFormByEmail() throws IOException {
         clearVbox();
         NoidungServiceImpl ndS = new NoidungServiceImpl();
-        List<Noidung> lstTask  = ndS.getAllByProjectAndEmail(Transdata.Instance().getProjectID(),txtSearch.getText());
+        List<Noidung> lstTask = ndS.getAllByProjectAndEmail(Transdata.Instance().getProjectID(), txtSearch.getText());
         NguoidungServicesImpl userS = new NguoidungServicesImpl();
         for (Noidung noidung : lstTask) {
             if (noidung.getTrangThai().equals(TRANGTHAITASK.CANLAM.toString())) {
@@ -133,7 +145,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbToDo.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -142,7 +154,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbInProgress.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -151,7 +163,7 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbSolved.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
@@ -160,25 +172,26 @@ public class DetailProjectController implements Initializable {
                 String userName = "";
                 List<Nguoidung> lstUser = userS.getAllByTask(noidung.getIDNoiDung());
                 for (Nguoidung nguoidung : lstUser) {
-                    userName = userName + nguoidung.getHoTen()+",";
+                    userName = userName + nguoidung.getHoTen() + ",";
                 }
                 vbComplete.getChildren().add(getNode(noidung.getIDNoiDung(), noidung.getTieuDe(), userName));
             }
         }
-        
+
     }
-    public Node getNode(String id, String tittle, String userName) throws IOException{
-         Node node = (Node) FXMLLoader.load(getClass().getResource("/Views/panelMission.fxml"));
-            Label lbID = (Label) node.lookup("#lbIDTask");
-            lbID.setText(id);
-            Label lbTitle = (Label) node.lookup("#lbTitleTask");
-            lbTitle.setText(tittle);
-            Label lbUserName = (Label) node.lookup("#lbUserName");
-            lbUserName.setText(userName);
-            return node;
+
+    public Node getNode(String id, String tittle, String userName) throws IOException {
+        Node node = (Node) FXMLLoader.load(getClass().getResource("/Views/panelMission.fxml"));
+        Label lbID = (Label) node.lookup("#lbIDTask");
+        lbID.setText(id);
+        Label lbTitle = (Label) node.lookup("#lbTitleTask");
+        lbTitle.setText(tittle);
+        Label lbUserName = (Label) node.lookup("#lbUserName");
+        lbUserName.setText(userName);
+        return node;
     }
-    
-    public void initData(Duan DA){
+
+    public void initData(Duan DA) {
 //        setDa(DA);
     }
 
@@ -194,8 +207,9 @@ public class DetailProjectController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+
     private void detailTask(MouseEvent event) throws IOException {
-         FXMLLoader fxmlLoader = new FXMLLoader();
+        FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/Views/AddMissionForProject.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage stage = new Stage();
@@ -222,7 +236,7 @@ public class DetailProjectController implements Initializable {
 //    }
     @FXML
     private void btnOpenDiary(MouseEvent event) throws IOException {
-         FXMLLoader fxmlLoader = new FXMLLoader();
+        FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/Views/NhatKy.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage stage = new Stage();
@@ -252,7 +266,7 @@ public class DetailProjectController implements Initializable {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
+
     }
 
     @FXML
@@ -267,5 +281,20 @@ public class DetailProjectController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-    
+     public class bindDataComboBoxMemBer {
+
+        public bindDataComboBoxMemBer(int id, String name) {
+            this.userID = id;
+            this.userName = name;
+        }
+
+        public int userID;
+        public String userName;
+
+        @Override
+        public String toString() {
+            return this.userName;
+        }
+    }
+
 }
